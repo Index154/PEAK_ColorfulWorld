9bf3626d8f964490c99fe6d63372420cd4ea5758
Index154 <flfc007@gmail.com>
2025-07-20 19:15:05 +0200

    Some changes

--- Patches.cs
+++ Patches.cs
@@ -1,6 +1,22 @@
+using HarmonyLib;
+using UnityEngine;
+
 namespace ColorfulWorld;
 
 public class Patches()
 {
-    
+    [HarmonyPatch(typeof(SetMaterialProperty))]
+    [HarmonyPatch("SetVal")]
+    [HarmonyPrefix]
+    public static bool Test(SetMaterialProperty __instance, ref float val)
+    {
+        Renderer component = __instance.GetComponent<Renderer>();
+		MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+		component.GetPropertyBlock(materialPropertyBlock);
+		materialPropertyBlock.SetFloat(__instance.propertyName, val);
+		component.SetPropertyBlock(materialPropertyBlock);
+
+        ColorfulWorld.Logger.LogInfo("! => " + component.material.color);
+        return false;
+    }
 }
